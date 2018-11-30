ff67af9fe9b33eda9eac5121b94921fcc0afdc19
Michał Kraciuk <42178544+Michalqq@users.noreply.github.com>
2018-11-30 16:03:21 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,18 +3,19 @@
    <head>
     <link rel="stylesheet" type="text/css" href="style2.css">
    </head>
-    <p style="margin-left:970px; top:30px">Moja sprzedaż:</p>
-    <p style="margin-left:970px; top:150px">Moje zakupy:</p>
-    <p style="margin-left:970px; top:300px">Historia działań:</p>
+    <p style="margin-left:780px; top:30px">Moja sprzedaż:</p>
+    <p style="margin-left:780px; top:150px">Moje zakupy:</p>
+    <p style="margin-left:780px; top:300px">Historia działań:</p>
     
     <body>
+    <div style="margin-top:-20px; height:45px; width:1900px;background-color:darkgrey"></div>
 <form method="post">
-    <p1 class="dateLabel" style = "left: 50px">Od daty:</p1>
-        <input type="date" class="dateLabel" style="left: 50px; top:35px" id="dataStart" name="dataStart">
-    <p1 class="dateLabel" style = "left: 200px">Do daty:</p1>
-        <input type="date" class="dateLabel" style="left: 200px; top:35px" id="dataStop" name="dataStop">
-    <input type="submit" name="mainTable" id="mainTable" style="position:absolute; margin-left:15px; top:50px; height:31px;"  value="Pokaż wszystkie elementy" /><br/>
-    <br>
+    <p1 class="dateLabel" style = "left: -130px">Od daty:</p1>
+        <input type="date" class="dateLabel" style="left: -130px; top:35px" id="dataStart" name="dataStart">
+    <p1 class="dateLabel" style = "left: 20px">Do daty:</p1>
+        <input type="date" class="dateLabel" style="left: 20px; top:35px" id="dataStop" name="dataStop">
+    <input type="submit" name="mainTable" id="mainTable" style="position:absolute; margin-left:15px; top:50px; height:31px;"  value="Pokaż wszystkie elementy" /><br>
+    <input type="submit" class="selectBuy" name="showHistory" id="showHistory" style="margin-left:510px; top: 329px; width: 120px; height: 30px;"  value="Pokaż" />
     <p1 style="left: 230px; position:absolute; top:55px"><b>FILTR:</b></p1> 
     <input id="filter" type="text" name="filter" style="position:absolute; height:25px; margin-left:295px; top:50px" value=""><br>
     <p1 style="left: 10px; position:absolute; top:15px"><b>Login:</b></p1> 
@@ -24,20 +25,19 @@
     <br>
     <input type="submit" class="selectBuy" name="buyBtn" id="buyBtn" style="margin-left:450px; top: 230px; width: 170px; height: 30px;"  value="Dodaj zakupione do bazy" />
     <input type="submit" class="selectBuy" name="confirmDeliverToPL" id="confirmDeliverToPL" style="margin-left:450px; top: 275px; width: 170px; height: 30px;"  value="Potwierdź dostawę do PL" />
-    <table id="sellTable" style="margin-left:970px; width:400px; position:absolute; top:80px">
+    <table id="sellTable" style="margin-left:780px; width:400px; position:absolute; top:80px">
     <tr class="header"><td style="width:150px">Nazwa</td></td><td>Cena sprzedaży</td><td>Pobranie</td><td>Kwota pobrania</td></tr>
     </table>
-    <table id="buyTable" style="margin-left:970px; width:400px; position:absolute; top:200px">
+    <table id="buyTable" style="margin-left:780px; width:400px; position:absolute; top:200px">
     <tr class="header"><td style="width:35%">Nazwa</td></td><td style="width:20%">Kwota zakupu</td><td>Ilość</td><td>Sprzedawca</td></tr>
     </table>
     <input type="text" class="select1" name="Sell_price" id="Sell_price" style="margin-left:150px; width: 70px;"/>
-    <input type="text" class="select1" name="Cash_on_delivery" id="Cash_on_delivery" style="margin-left:323px; width: 70px; background-color:#dddddd" readonly/>
+    <input type="text" class="select1" name="Cash_on_delivery" id="Cash_on_delivery" style="margin-left:325px; width: 70px; background-color:#dddddd" readonly/>
     <input type="submit" class="select1" name="sellBtn" id="sellBtn" style="margin-left:450px; width: 170px; height: 30px;" value="Sprzedaj" />
-    <input type="text" class="selectBuy" name="Buy_price" id="Buy_price" style="margin-left:140px; width: 70px;"/> 
-    <input type="text" class="selectBuy" name="Quantity" id="Quantity" style="margin-left:220px; width: 50px;"/>
-    <input type="checkbox" class="select1" name="If_cash_on_delivery" id="If_cash_on_delivery" style="margin-left:240px; width: 70px;" value="0" onclick="CashOnDelivery()" >
-    <input type="submit" class="selectBuy" name="showHistory" id="showHistory" style="margin-left:500px; top: 329px; width: 120px; height: 30px;"  value="Pokaż" />
-    
+    <input type="text" class="selectBuy" name="Buy_price" id="Buy_price" style="margin-left:145px; width: 70px;"/> 
+    <input type="text" class="selectBuy" name="Quantity" id="Quantity" style="margin-left:223px; width: 50px;"/>
+    <input type="checkbox" class="select1" name="If_cash_on_delivery" id="If_cash_on_delivery" style="margin-left:235px; width: 70px; margin-top:-0px" value="0" onclick="CashOnDelivery()" >
+      
 <!--</form>-->
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
@@ -51,7 +51,6 @@ function CashOnDelivery() {
         document.getElementById("Cash_on_delivery").readOnly = true;
         document.getElementById("Cash_on_delivery").style.backgroundColor = '#dddddd';
     }
-    
 }
 window.onload = function getTodayDate() {
     let data = new Date;
@@ -72,11 +71,6 @@ function lacz_bd() {
         die('Connect Error: ' . $db->connect_error);
         return false;
     }
-    if (! $db) {
-        echo "Błąd logowania";
-        exit();
-        return false;
-    }
    $db->autocommit(TRUE);
    return $db;
 }
@@ -86,27 +80,18 @@ function query_DB($db, $sql) {
 }
 function select_From_DB($Select) {
 //połaczenie z bazą
-$rowColor = array("#ffffff","#e1f2e1");
+$rowColor = array("#ffffff","#e1f2e1", "#FFFF9F");
 $db = lacz_bd();
-//zapytanie sql do bazy określające jakie dane mają zostać pobrane
-//$zapytanie = "SELECT * FROM wskazniki ORDER BY Buy_date";
-//pobranie wyniku zapytania
-//$wynik = $db->query($zapytanie);
 $wynik= query_DB($db, "SELECT * FROM wskazniki ORDER BY Buy_date");
-//obliczanie ilości rekordów
 $ile_znalezionych = $wynik->num_rows;
-//rozpoczynamy budowanie tabeli dla naszych danych
-StartTable("mainTable");
+StartTable("mainTable", "mainTableCSS");
 $parameter = array ("", "In", "Nazwa", "Cena zakupu", "Ilość", "Data zakupu", "Ile zam.");
 AddTableRow(7, "header", $parameter);
-//pętla po rekordach z bazy
 $index = 0;
 $namesIndex = array();
 for ($i=0; $i <$ile_znalezionych; $i++) { // Create main table
     $temp = 0;
     $wiersz = $wynik->fetch_assoc();
-    //$zapytanie3 = "SELECT * FROM item WHERE Name LIKE '%$Select%' AND ID=".$wiersz['Item_ID'];
-    //$wynik3 = $db->query($zapytanie3);
     $wynik3= query_DB($db, "SELECT * FROM item WHERE Name LIKE '%$Select%' AND ID=".$wiersz['Item_ID']);
     $howRekordsDelivered = ($db->query("SELECT * FROM wskazniki WHERE Item_ID=".$wiersz['Item_ID']." AND delivered_to_Poland=1 AND Sell_price IS NULL"))->num_rows;
     $howRekordsOnDelivery = ($db->query("SELECT * FROM wskazniki WHERE Item_ID=".$wiersz['Item_ID']." AND delivered_to_Poland IS NULL"))->num_rows;
@@ -116,7 +101,9 @@ for ($i=0; $i <$ile_znalezionych; $i++) { // Create main table
         if ($temp != 1) {  
             array_push($namesIndex, $wiersz3['Name']);
             $index += 1;
-            echo '<tr style="background-color:'.$rowColor[($index % 2)].';hover {background-color: yellow}">';
+            $colorIndex = ($index % 2);
+            if ($howRekordsDelivered < 2 AND $howRekordsOnDelivery<1) $colorIndex=2;
+            echo '<tr style="background-color:'.$rowColor[$colorIndex].';hover {background-color: yellow}">';
             echo '<td></td>';
             echo '<td style="width:2%">'.$index.'</td>';
             $zapytanie2 = "SELECT Name FROM item WHERE ID=".$wiersz['Item_ID'];
@@ -132,13 +119,12 @@ for ($i=0; $i <$ile_znalezionych; $i++) { // Create main table
     }
 }
 echo '</table>';
-echo json_encode($namesIndex);
+//echo json_encode($namesIndex);
 echo '<select class="select1" name="select1">';
 for ($i=0; $i <count ($namesIndex); $i++) {
     echo '<option>'.$namesIndex[$i].'</option>';
 }
 echo '</select>';
-
 echo '<select class="selectBuy" name="selectBuy">';
 for ($i=0; $i <count ($namesIndex); $i++) {
     echo '<option>'.$namesIndex[$i].'</option>';
@@ -166,13 +152,16 @@ if (isset($_POST['sellBtn'])) { // Update record when SELL item
         $selectItemId = "SELECT ID FROM item WHERE Name = '".$_POST['select1']."'";
         $wynik0 = $db->query($selectItemId);
         $itemID = $wynik0->fetch_assoc();
-        $selectOneItem = "SELECT ID FROM wskazniki WHERE delivered_to_Poland = 1 AND Item_ID = '".$itemID['ID']."'";
+        $selectOneItem = "SELECT ID FROM wskazniki WHERE delivered_to_Poland = 1 AND Sell_price IS NULL AND Item_ID = '".$itemID['ID']."'";
         $wynik00 = $db->query($selectOneItem);
         $ID = $wynik00->fetch_assoc();
-        $today = getdate();
+        $today = getdate();  
         $todayDate = $today['year']."-".$today['mon']."-".$today['mday'];
+        $todayFullDate = $today['year']."-".$today['mon']."-".$today['mday']." ".$today['hours'].":".$today['minutes'].":".$today['seconds'].":";
         //$updateSellValue = "UPDATE wskazniki SET Sell_price=NULL ,Sell_date = NULL";
-        $updateSellValue = "UPDATE wskazniki SET Sell_price=".$_POST['Sell_price'].", Sell_date='".$todayDate."', Last_action_date='".$todayDate."' WHERE ID=".$ID['ID'];
+        if ($_POST['Cash_on_delivery']=="") $sql = "UPDATE wskazniki SET Sell_price=".$_POST['Sell_price'].", Sell_date='".$todayDate."', Last_action_date='".$todayFullDate."', If_cash_on_delivery = 1, Cash_on_delivery=".$_POST['Cash_on_delivery']." WHERE ID=".$ID['ID'];
+        else $sql = "UPDATE wskazniki SET Sell_price=".$_POST['Sell_price'].", Sell_date='".$todayDate."', Last_action_date='".$todayFullDate."', If_cash_on_delivery = 1, Cash_on_delivery=".$_POST['Cash_on_delivery']." WHERE ID=".$ID['ID'];
+        $updateSellValue = $sql;
         if ($db->query($updateSellValue)=== TRUE ) {
             echo "Record updated successfully";
         } else {
@@ -197,7 +186,7 @@ if (isset($_POST['buyBtn'])) { // Set record when BUY item
         $sellerID = $wynik01->fetch_assoc();
         $today = getdate();
         $todayDate = $today['year']."-".$today['mon']."-".$today['mday'];
-        echo $todayDate;
+        //echo $todayDate;
         $singleItemPrice = ($_POST['Buy_price']) / ($_POST['Quantity']);
         for ($i=0; $i<$_POST['Quantity']; $i++) {
             $insertSQL = "INSERT INTO wskazniki (Item_ID, Buy_date, Buy_price, seller_ID, Last_action_date) VALUES (".$itemID['ID'].", '".$todayDate."', ".$singleItemPrice.", ".$sellerID['ID'].", '".$todayDate."')";
@@ -226,6 +215,7 @@ if (isset($_POST['confirmDeliverToPL'])) { // Update record when item arrived to
         $ile_znalezionych = $wynik00->num_rows;
         $today = getdate();
         $todayDate = $today['year']."-".$today['mon']."-".$today['mday'];
+         $todayFullDate = $today['year']."-".$today['mon']."-".$today['mday']." ".$today['hours'].":".$today['minutes'].":".$today['seconds'].":";
         if ($ile_znalezionych < $_POST['Quantity']) {
             echo ("Elementów w transporcie jest mniej niż potwierdzasz");
             closeDB($db);
@@ -233,7 +223,7 @@ if (isset($_POST['confirmDeliverToPL'])) { // Update record when item arrived to
         } else {
             for ($i=0; $i<$_POST['Quantity']; $i++) {
                 $ID = $wynik00->fetch_assoc();
-                $updateDeliveryToPL = "UPDATE wskazniki SET delivered_to_Poland=1, Last_action_date='".$todayDate."' WHERE ID=".$ID['ID'];
+                $updateDeliveryToPL = "UPDATE wskazniki SET delivered_to_Poland=1, Last_action_date='".$todayFullDate."' WHERE ID=".$ID['ID'];
                 if ($db->query($updateDeliveryToPL)=== TRUE ) {
                     echo "Record updated successfully  </br>";
                 } else {
@@ -246,39 +236,45 @@ if (isset($_POST['confirmDeliverToPL'])) { // Update record when item arrived to
     }
 }
 if (isset($_POST['showHistory'])) { // Show action history 
+    historyTable("SELECT * FROM wskazniki WHERE Last_action_date BETWEEN '".$_POST['dataStart']." 00:00:00' AND '".$_POST['dataStop']." 23:59:59' ORDER BY Last_action_date DESC");
+    select_From_DB($_POST['filter']);
+}
+function historyTable($sqlParam){
 $rowColor = array("#ffffff","#e1f2e1");
 $db = lacz_bd();
-$wynik= query_DB($db, "SELECT * FROM wskazniki WHERE Last_action_date BETWEEN '".$_POST['dataStart']."' AND '".$_POST['dataStop']."'");
+$wynik= query_DB($db, $sqlParam);
 $ile_znalezionych = $wynik->num_rows;
-    echo '<div class="hisTable">';
-StartTable("hisTable");
-$parameter = array ("In", "Nazwa", "Cena zakupu", "Cena sprzedaży", "Data zakupu", "Data sprzedaży", "Dostarczone do PL?", "Kwota pobrania");
-AddTableRow(8, "hisTable", $parameter);
+    echo '<div class="hisTableCSS">';
+StartTable("hisTable", 'hisTableCSS');
+$parameter = array ("In", "Nazwa", "Cena zakupu","Data zakupu", "Cena sprzedaży", "Data sprzedaży", "Dstrcz do PL?", "Kwota pobran", "Ostatnio");
+AddTableRow(9, "header1", $parameter);
 $index = 1;
 for ($i=0; $i <$ile_znalezionych; $i++) { // Create main table
     $temp = 0;
     $wiersz = $wynik->fetch_assoc();
-    echo '<tr style="background-color:'.$rowColor[($index % 2)].';hover {background-color: yellow}">';
+    $wynik2= query_DB($db, "SELECT Name FROM item WHERE ID=".$wiersz['Item_ID']);
+    $wiersz2 = $wynik2->fetch_assoc();
+    echo '<tr style="font-size:11px; background-color:'.$rowColor[($index % 2)].';hover {background-color: yellow}">';
     echo '<td>'.$index.'</td>';
-    echo '<td style="width:10px">'.$wiersz['Item_ID'].'</td>';
-    echo '<td>'.$wiersz['Buy_price'].'</td>';
-    echo '<td style="width:150px">'.$wiersz['Sell_price'].'</td>';
-    echo '<td>'.$wiersz['Buy_date'].'</td>';
-    echo '<td>'.$wiersz['Sell_date'].'</td>';
-    echo '<td style="width:10px">'.$wiersz['delivered_to_Poland'].'</td>';
-    echo '<td style="width:10px">'.$wiersz['Cash_on_delivery'].'</td>';
+    echo '<td style="width:35%">'.$wiersz2['Name'].'</td>';
+    echo '<td style="width:10%">'.$wiersz['Buy_price'].'</td>';
+    echo '<td style="width:14%">'.$wiersz['Buy_date'].'</td>';
+    echo '<td style="width:10%">'.$wiersz['Sell_price'].'</td>';
+    echo '<td style="width:14%">'.$wiersz['Sell_date'].'</td>';
+    echo '<td style="width:5%">'.$wiersz['delivered_to_Poland'].'</td>';
+    echo '<td>'.$wiersz['Cash_on_delivery'].'</td>';
+    echo '<td style="width:14%">'.$wiersz['Last_action_date'].'</td>';
     echo '</tr>';
     $index += 1;
     }
-
 echo '</table>';  
-echo '</div>';
+echo '</div>'; 
 }
 function closeDB($db) {
     mysqli_close($db);
 }
-function StartTable($tableID) {
-    echo '<table id='.$tableID.'>' ;
+function StartTable($tableID, $tableClass) {
+    echo '<table id='.$tableID.' class= '.$tableClass.'>' ;
 }
 function AddTableRow(int $index, $class, $parameter) {
     echo '<tr class='.$class.'>';
