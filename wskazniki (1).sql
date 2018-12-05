941e75f44ae0da4e83766a9502cb33fbefaa7c73
Michał Kraciuk <42178544+Michalqq@users.noreply.github.com>
2018-12-05 14:18:41 +0100

    Add files via upload

---
+++ wskazniki (1).sql
@@ -0,0 +1,319 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 05 Gru 2018, 14:18
+-- Wersja serwera: 10.1.37-MariaDB
+-- Wersja PHP: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `wskazniki`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `item`
+--
+
+CREATE TABLE `item` (
+  `ID` int(11) NOT NULL,
+  `Name` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `item`
+--
+
+INSERT INTO `item` (`ID`, `Name`) VALUES
+(1, 'ADDCO BOOST'),
+(2, 'ADDCO OilPressure'),
+(3, 'ADDCO OilTemp'),
+(4, 'ADDCO EGT'),
+(5, 'ADDCO AFR'),
+(6, 'ADDCO WaterTemp'),
+(10, 'DEFI Boost'),
+(11, 'DEFI OilPressure'),
+(12, 'DEFI OilTemp'),
+(13, 'DEFI EGT'),
+(20, 'Greddy Boost'),
+(21, 'Greddy OilPressure'),
+(22, 'Greddy OilTemp'),
+(23, 'Greddy EGT'),
+(30, 'Nakladki 4 x BREMBO Red'),
+(31, 'Nakladki 4 x BREMBO Blue'),
+(32, 'Nakladki 4 x BREMBO Yellow'),
+(33, 'Nakladki 4 x BREMBO Gold'),
+(50, 'Lotki male 4 szt.'),
+(51, 'Lotki duze 6 szt.'),
+(52, 'Lotki plaskie plastik 4 szt.'),
+(60, 'Blow Off 1.8t'),
+(61, 'Adaptery skretu BMW E36 black'),
+(70, 'Obudowa plaska potrojna 3x52mm'),
+(71, 'Obudowa na slupek potrojna 3x52mm'),
+(80, 'MP3 AUX USB Adapter 8PIN'),
+(90, 'Podstawka pod filtr oleju'),
+(98, 'Dystans 5mm'),
+(99, 'Daszek maly 52mm'),
+(100, 'OilPressure sensor czujnik'),
+(101, 'Boost sensor czujnik map'),
+(102, 'EGT sensor czujnik termopara'),
+(109, 'Trojnik M12x1.5 x 1/8NPT'),
+(110, 'Adapter do turbo wezyk Quick tap');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `seller`
+--
+
+CREATE TABLE `seller` (
+  `ID` int(11) NOT NULL,
+  `Seller_name` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `seller`
+--
+
+INSERT INTO `seller` (`ID`, `Seller_name`) VALUES
+(1, 'PIVOT CAR ELECTRICAL PARTS'),
+(2, 'HANSSENTUNE Racing Store'),
+(3, 'LZONE PERFORMANCE Store'),
+(4, 'HUBsport DE Store'),
+(5, 'Performance Tuning Parts Store'),
+(6, 'Car Top ACE Store'),
+(7, 'REPLAITZ Store'),
+(8, 'EPMAN Racing Store'),
+(9, 'PQY RACING Store'),
+(10, 'banwinoto Official Store'),
+(11, 'Minespeed Store'),
+(12, 'TOSPORT RACING Store'),
+(13, 'WLR racing Store'),
+(14, 'WarmHome Store'),
+(15, 'MaxPeedingRods - official from webpage'),
+(16, 'TeamOne Store'),
+(17, 'CoCo Car Parts'),
+(18, 'Awesome for you store');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `wskazniki`
+--
+
+CREATE TABLE `wskazniki` (
+  `ID` int(11) NOT NULL,
+  `Item_ID` int(11) NOT NULL,
+  `Buy_date` date NOT NULL,
+  `Sell_date` date DEFAULT NULL,
+  `Buy_price` float NOT NULL,
+  `Cash_on_delivery` float DEFAULT NULL,
+  `If_cash_on_delivery` tinyint(1) DEFAULT NULL,
+  `Sell_price` float DEFAULT NULL,
+  `seller_ID` int(11) NOT NULL,
+  `delivered_to_Poland` int(11) DEFAULT NULL,
+  `Last_action_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `wskazniki`
+--
+
+INSERT INTO `wskazniki` (`ID`, `Item_ID`, `Buy_date`, `Sell_date`, `Buy_price`, `Cash_on_delivery`, `If_cash_on_delivery`, `Sell_price`, `seller_ID`, `delivered_to_Poland`, `Last_action_date`) VALUES
+(1, 30, '2018-11-07', NULL, 27.8075, NULL, NULL, NULL, 5, NULL, NULL),
+(2, 1, '2018-11-23', NULL, 59.715, NULL, NULL, NULL, 1, NULL, NULL),
+(3, 1, '2018-11-23', NULL, 59.715, NULL, NULL, NULL, 1, NULL, NULL),
+(4, 1, '2018-11-23', NULL, 59.715, NULL, NULL, NULL, 1, NULL, NULL),
+(5, 1, '2018-11-23', NULL, 59.715, NULL, NULL, NULL, 1, NULL, NULL),
+(6, 10, '2218-11-23', NULL, 41, NULL, NULL, NULL, 1, NULL, NULL),
+(7, 4, '2218-11-25', NULL, 41, NULL, NULL, NULL, 1, NULL, NULL),
+(8, 30, '2018-11-07', NULL, 27.8075, NULL, NULL, NULL, 5, NULL, NULL),
+(9, 30, '2018-11-07', NULL, 27.8075, NULL, NULL, NULL, 5, NULL, NULL),
+(10, 30, '2018-11-07', NULL, 27.8075, NULL, NULL, NULL, 5, NULL, NULL),
+(11, 30, '2018-10-30', NULL, 29.0475, NULL, NULL, NULL, 5, 1, NULL),
+(12, 30, '2018-10-30', NULL, 29.0475, NULL, NULL, NULL, 5, 1, NULL),
+(13, 30, '2018-10-29', NULL, 28.8925, NULL, NULL, NULL, 5, 1, NULL),
+(14, 30, '2018-10-29', NULL, 28.8925, NULL, NULL, NULL, 5, 1, NULL),
+(15, 31, '2018-10-29', NULL, 28.8925, NULL, NULL, NULL, 5, 1, NULL),
+(16, 33, '2018-09-21', NULL, 34.835, NULL, NULL, NULL, 5, 1, NULL),
+(17, 33, '2018-09-21', NULL, 34.835, NULL, NULL, NULL, 5, 1, NULL),
+(18, 33, '2018-09-21', NULL, 34.835, NULL, NULL, NULL, 5, 1, NULL),
+(19, 33, '2018-09-21', NULL, 34.835, NULL, NULL, NULL, 5, 1, NULL),
+(20, 52, '2018-09-26', NULL, 18.478, NULL, NULL, NULL, 13, 1, NULL),
+(21, 52, '2018-09-26', NULL, 18.478, NULL, NULL, NULL, 13, 1, NULL),
+(22, 52, '2018-09-26', NULL, 18.478, NULL, NULL, NULL, 13, 1, NULL),
+(23, 52, '2018-09-26', NULL, 18.478, NULL, NULL, NULL, 13, 1, NULL),
+(24, 52, '2018-09-26', NULL, 18.478, NULL, NULL, NULL, 13, 1, NULL),
+(25, 50, '2018-10-18', NULL, 9.5775, NULL, NULL, NULL, 10, 1, NULL),
+(26, 50, '2018-10-18', NULL, 9.5775, NULL, NULL, NULL, 10, 1, NULL),
+(27, 50, '2018-10-18', NULL, 9.5775, NULL, NULL, NULL, 10, 1, NULL),
+(28, 50, '2018-10-18', NULL, 9.5775, NULL, NULL, NULL, 10, 1, NULL),
+(29, 51, '2018-10-18', NULL, 11.3525, NULL, NULL, NULL, 9, 1, NULL),
+(30, 51, '2018-10-18', NULL, 11.3525, NULL, NULL, NULL, 9, 1, NULL),
+(31, 51, '2018-10-18', NULL, 11.3525, NULL, NULL, NULL, 9, 1, NULL),
+(32, 51, '2018-10-18', NULL, 11.3525, NULL, NULL, NULL, 9, 1, NULL),
+(36, 98, '2018-04-19', NULL, 47.413, NULL, NULL, NULL, 17, 1, NULL),
+(37, 100, '2018-08-05', '2018-11-28', 26, NULL, NULL, 51.52, 1, 1, NULL),
+(38, 100, '2018-08-05', NULL, 26, NULL, NULL, NULL, 1, 1, NULL),
+(39, 5, '2018-08-05', NULL, 73.856, NULL, NULL, NULL, 1, 1, NULL),
+(40, 5, '2018-08-05', NULL, 73.856, NULL, NULL, NULL, 1, 1, NULL),
+(41, 5, '2018-08-05', NULL, 73.856, NULL, NULL, NULL, 1, 1, NULL),
+(42, 6, '2018-08-06', NULL, 73.6, NULL, NULL, NULL, 1, 1, NULL),
+(43, 90, '2018-08-22', NULL, 35.62, NULL, NULL, NULL, 1, 1, NULL),
+(44, 90, '2018-08-22', NULL, 35.62, NULL, NULL, NULL, 1, 1, NULL),
+(45, 90, '2018-08-22', NULL, 35.62, NULL, NULL, NULL, 1, 1, NULL),
+(46, 110, '2018-08-28', NULL, 10.692, NULL, NULL, NULL, 4, 1, NULL),
+(47, 110, '2018-08-28', NULL, 10.692, NULL, NULL, NULL, 4, 1, NULL),
+(48, 110, '2018-08-28', NULL, 10.692, NULL, NULL, NULL, 4, 1, NULL),
+(49, 110, '2018-08-28', NULL, 10.692, NULL, NULL, NULL, 4, 1, NULL),
+(50, 110, '2018-08-28', NULL, 10.692, NULL, NULL, NULL, 4, 1, NULL),
+(51, 21, '2018-08-31', NULL, 126.651, NULL, NULL, NULL, 2, 1, NULL),
+(52, 80, '2018-09-10', '2018-11-29', 81.365, NULL, NULL, 150.4, 6, 3, '2018-12-03 00:09:10'),
+(53, 4, '2018-09-25', NULL, 67.8725, NULL, NULL, NULL, 1, 1, NULL),
+(54, 4, '2018-09-25', NULL, 67.8725, NULL, NULL, NULL, 1, 1, NULL),
+(55, 109, '2018-09-26', NULL, 25.88, NULL, NULL, NULL, 12, 1, NULL),
+(56, 109, '2018-09-26', NULL, 25.88, NULL, NULL, NULL, 12, 1, NULL),
+(57, 109, '2018-09-26', NULL, 25.88, NULL, NULL, NULL, 12, 1, NULL),
+(58, 109, '2018-09-26', NULL, 25.88, NULL, NULL, NULL, 12, 1, NULL),
+(59, 109, '2018-09-26', NULL, 25.88, NULL, NULL, NULL, 12, 1, NULL),
+(60, 70, '2018-09-27', '2018-11-28', 10.843, NULL, NULL, 27.6, 11, 1, NULL),
+(61, 70, '2018-09-27', NULL, 10.843, NULL, NULL, NULL, 11, 1, NULL),
+(62, 3, '2018-10-03', '2018-12-05', 78.11, NULL, NULL, 101.78, 1, 3, '2018-12-05 08:50:04'),
+(63, 3, '2018-10-03', NULL, 78.11, NULL, NULL, NULL, 1, 1, NULL),
+(64, 3, '2018-10-03', NULL, 78.11, NULL, NULL, NULL, 1, 1, NULL),
+(65, 12, '2018-10-03', NULL, 100.265, NULL, NULL, NULL, 8, 1, NULL),
+(66, 12, '2018-10-03', NULL, 100.265, NULL, NULL, NULL, 8, 1, NULL),
+(67, 1, '2018-10-08', '2018-12-05', 62.045, NULL, NULL, 127.98, 1, 3, '2018-12-05 08:49:28'),
+(68, 60, '2018-10-08', '2018-11-28', 51.086, NULL, NULL, 110.4, 3, 1, NULL),
+(69, 20, '2018-10-10', '2018-12-03', 103, NULL, NULL, 172.7, 2, 3, '2018-12-03 09:14:21'),
+(70, 20, '2018-10-16', NULL, 134.525, NULL, NULL, NULL, 2, 1, NULL),
+(71, 20, '2018-10-16', NULL, 134.525, NULL, NULL, NULL, 2, 1, NULL),
+(72, 80, '2018-10-23', '2018-12-03', 83.3225, 177.5, 1, 150.4, 6, 2, '2018-12-03 09:11:19'),
+(73, 80, '2018-10-23', NULL, 83.3225, NULL, NULL, NULL, 6, 1, NULL),
+(74, 80, '2018-10-23', NULL, 83.3225, NULL, NULL, NULL, 6, 1, NULL),
+(75, 80, '2018-10-27', NULL, 83.3225, NULL, NULL, NULL, 6, 1, NULL),
+(76, 80, '2018-10-27', NULL, 83.3225, NULL, NULL, NULL, 6, 1, NULL),
+(77, 80, '2018-10-27', NULL, 83.3225, NULL, NULL, NULL, 6, 1, NULL),
+(78, 80, '2018-10-27', NULL, 83.3225, NULL, NULL, NULL, 6, 1, NULL),
+(79, 1, '2018-10-27', '2018-12-05', 85.2325, 160, 1, 139, 1, 2, '2018-12-05 08:51:35'),
+(80, 23, '2018-10-30', NULL, 128.395, NULL, NULL, NULL, 2, NULL, NULL),
+(81, 23, '2018-10-30', NULL, 128.395, NULL, NULL, NULL, 2, NULL, NULL),
+(82, 2, '2018-10-30', '2018-11-28', 65.4925, NULL, NULL, 105.8, 1, 1, NULL),
+(83, 2, '2018-10-30', '2018-12-05', 65.4925, NULL, NULL, 0, 1, 3, '2018-12-05 08:33:42'),
+(84, 2, '2018-10-30', NULL, 65.4925, NULL, NULL, NULL, 1, 1, NULL),
+(85, 2, '2018-10-30', NULL, 65.4925, NULL, NULL, NULL, 1, 1, NULL),
+(86, 101, '2018-11-07', '2018-11-29', 20.774, NULL, NULL, 64.4, 4, 1, '2018-11-29 00:00:00'),
+(87, 101, '2018-11-07', '2018-12-05', 20.774, 87.5, 1, 64.4, 4, 2, '2018-12-05 08:43:31'),
+(88, 101, '2018-11-07', NULL, 20.774, NULL, NULL, NULL, 4, 1, '2018-11-29 00:00:00'),
+(89, 101, '2018-11-07', NULL, 20.774, NULL, NULL, NULL, 4, 1, '2018-11-29 00:00:00'),
+(90, 101, '2018-11-07', NULL, 20.774, NULL, NULL, NULL, 4, 1, '2018-11-29 00:00:00'),
+(91, 4, '2018-11-11', NULL, 66.6175, NULL, NULL, NULL, 1, NULL, NULL),
+(92, 4, '2018-11-11', NULL, 66.6175, NULL, NULL, NULL, 1, NULL, NULL),
+(93, 4, '2018-11-11', NULL, 66.6175, NULL, NULL, NULL, 1, NULL, NULL),
+(94, 4, '2018-11-11', NULL, 66.6175, NULL, NULL, NULL, 1, NULL, NULL),
+(95, 1, '2018-11-11', NULL, 59.7975, NULL, NULL, NULL, 1, NULL, NULL),
+(96, 1, '2018-11-11', NULL, 59.7975, NULL, NULL, NULL, 1, NULL, NULL),
+(97, 1, '2018-11-11', NULL, 59.7975, NULL, NULL, NULL, 1, NULL, NULL),
+(98, 1, '2018-11-11', NULL, 59.7975, NULL, NULL, NULL, 1, NULL, NULL),
+(99, 100, '2018-11-13', NULL, 22.405, NULL, NULL, NULL, 1, NULL, NULL),
+(100, 100, '2018-11-13', NULL, 22.405, NULL, NULL, NULL, 1, NULL, NULL),
+(101, 100, '2018-11-13', NULL, 22.405, NULL, NULL, NULL, 1, NULL, NULL),
+(102, 100, '2018-11-13', NULL, 22.405, NULL, NULL, NULL, 1, NULL, NULL),
+(103, 60, '2018-11-13', NULL, 52.383, NULL, NULL, NULL, 3, NULL, NULL),
+(104, 60, '2018-11-13', NULL, 52.383, NULL, NULL, NULL, 3, NULL, NULL),
+(105, 60, '2018-11-13', NULL, 52.383, NULL, NULL, NULL, 3, NULL, NULL),
+(106, 22, '2018-11-13', NULL, 100.75, NULL, NULL, NULL, 2, NULL, NULL),
+(107, 22, '2018-11-13', NULL, 100.75, NULL, NULL, NULL, 2, NULL, NULL),
+(108, 1, '2018-11-14', NULL, 65.33, NULL, NULL, NULL, 1, NULL, NULL),
+(109, 1, '2018-11-14', NULL, 65.33, NULL, NULL, NULL, 1, NULL, NULL),
+(110, 1, '2018-11-14', NULL, 65.33, NULL, NULL, NULL, 1, NULL, NULL),
+(111, 1, '2018-11-14', NULL, 65.33, NULL, NULL, NULL, 1, NULL, NULL),
+(112, 10, '2018-11-15', NULL, 85.965, NULL, NULL, NULL, 1, NULL, NULL),
+(113, 10, '2018-11-15', NULL, 85.965, NULL, NULL, NULL, 1, NULL, NULL),
+(114, 13, '2018-11-15', NULL, 90.4, NULL, NULL, NULL, 1, NULL, NULL),
+(115, 13, '2018-11-15', NULL, 90.4, NULL, NULL, NULL, 1, NULL, NULL),
+(116, 102, '2018-10-26', NULL, 25.894, NULL, NULL, NULL, 14, 1, NULL),
+(117, 102, '2018-10-26', NULL, 25.894, NULL, NULL, NULL, 14, 1, NULL),
+(118, 102, '2018-10-26', NULL, 25.894, NULL, NULL, NULL, 14, 1, NULL),
+(119, 102, '2018-10-26', NULL, 25.894, NULL, NULL, NULL, 14, 1, NULL),
+(120, 11, '2018-11-27', NULL, 83.045, NULL, NULL, NULL, 8, NULL, NULL),
+(121, 11, '2018-11-27', NULL, 83.045, NULL, NULL, NULL, 8, NULL, NULL),
+(122, 4, '2218-11-21', NULL, 41, NULL, NULL, NULL, 1, NULL, NULL),
+(123, 60, '2018-11-29', NULL, 51.0775, NULL, NULL, NULL, 3, NULL, '2018-11-29 00:00:00'),
+(124, 60, '2018-11-29', NULL, 51.0775, NULL, NULL, NULL, 3, NULL, '2018-11-29 00:00:00'),
+(125, 60, '2018-11-29', NULL, 51.0775, NULL, NULL, NULL, 3, NULL, '2018-11-29 00:00:00'),
+(126, 60, '2018-11-29', NULL, 51.0775, NULL, NULL, NULL, 3, NULL, '2018-11-29 00:00:00'),
+(127, 98, '2018-11-29', NULL, 2, NULL, NULL, NULL, 1, NULL, '2018-11-29 00:00:00'),
+(138, 80, '2018-12-03', NULL, 85.045, NULL, NULL, NULL, 6, NULL, '2018-12-03 09:20:14'),
+(139, 80, '2018-12-03', NULL, 85.045, NULL, NULL, NULL, 6, NULL, '2018-12-03 09:20:14'),
+(140, 80, '2018-12-03', NULL, 85.045, NULL, NULL, NULL, 6, NULL, '2018-12-03 09:20:14'),
+(141, 80, '2018-12-03', NULL, 85.045, NULL, NULL, NULL, 6, NULL, '2018-12-03 09:20:14'),
+(142, 70, '2018-12-03', NULL, 8.914, NULL, NULL, NULL, 11, NULL, '2018-12-03 09:28:48'),
+(143, 70, '2018-12-03', NULL, 8.914, NULL, NULL, NULL, 11, NULL, '2018-12-03 09:28:48'),
+(144, 70, '2018-12-03', NULL, 8.914, NULL, NULL, NULL, 11, NULL, '2018-12-03 09:28:48'),
+(145, 70, '2018-12-03', NULL, 8.914, NULL, NULL, NULL, 11, NULL, '2018-12-03 09:28:48'),
+(146, 70, '2018-12-03', NULL, 8.914, NULL, NULL, NULL, 11, NULL, '2018-12-03 09:28:48'),
+(147, 61, '2018-12-05', NULL, 108.27, NULL, NULL, NULL, 9, NULL, '2018-12-05 14:01:47'),
+(148, 61, '2018-12-05', NULL, 108.27, NULL, NULL, NULL, 9, NULL, '2018-12-05 14:01:47');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `seller`
+--
+ALTER TABLE `seller`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `wskazniki`
+--
+ALTER TABLE `wskazniki`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `item`
+--
+ALTER TABLE `item`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+
+--
+-- AUTO_INCREMENT dla tabeli `seller`
+--
+ALTER TABLE `seller`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT dla tabeli `wskazniki`
+--
+ALTER TABLE `wskazniki`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
